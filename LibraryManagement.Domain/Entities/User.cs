082ad5e6557519faa93c6d2c4458aa65f5dfaebd
Kamile Sude Toprak <kamilesudetoprak@gmail.com>
2026-04-01 11:54:46 +0300

    Users sınıfına roleıd eklendi

--- LibraryManagement.Domain/Entities/User.cs
+++ LibraryManagement.Domain/Entities/User.cs
@@ -11,6 +11,9 @@ namespace LibraryManagement.Domain.Entities
         public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
 
+        public int RoleId { get; set; }
+        public Role? Role { get; set; }
+
         public ICollection<Rental>? Rentals { get; set; } = new List<Rental>();
     }
 }
