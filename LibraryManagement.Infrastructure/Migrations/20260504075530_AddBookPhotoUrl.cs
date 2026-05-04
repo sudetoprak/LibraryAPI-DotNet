790f4db41b7f4ca33db691f411b58951825d7dad
Kamile Sude Toprak <kamilesudetoprak@gmail.com>
2026-05-04 11:01:16 +0300

    Kitap kapak fotoğrafı için gerekli  migration eklendi

---
+++ LibraryManagement.Infrastructure/Migrations/20260504075530_AddBookPhotoUrl.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookPhotoUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 5, 4, 10, 55, 30, 201, DateTimeKind.Local).AddTicks(4526));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 5, 4, 10, 55, 30, 201, DateTimeKind.Local).AddTicks(4539));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2026, 5, 4, 10, 55, 30, 201, DateTimeKind.Local).AddTicks(4541));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "Books");
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 5, 2, 14, 53, 38, 967, DateTimeKind.Local).AddTicks(6622));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 5, 2, 14, 53, 38, 967, DateTimeKind.Local).AddTicks(6637));
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2026, 5, 2, 14, 53, 38, 967, DateTimeKind.Local).AddTicks(6640));
+        }
+    }
+}
